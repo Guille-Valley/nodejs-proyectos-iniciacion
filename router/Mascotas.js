e1d254187f1e0d0f72948e374f65a4d5d8f52090
Guille Valley <guille.valley@outlook.com>
2021-11-14 23:14:45 +0100

    variables de entorno en heroku

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('mascotas', { 
-        arrayMascotas:[
+
+const Mascota = require('../models/mascota')
+
+router.get('/', async (req, res) =>{
+    try {
+        
+        const arrayMascotasDB = await Mascota.find()
+        console.log(arrayMascotasDB)
+        res.render('mascotas', {
+            arrayMascotas: arrayMascotasDB
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+/* router.get('/', (req, res) => {
+    res.render('mascotas', {
+        arrayMascotas: [
             {
-                id: '000', nombre: 'Ventormenta', descripcion: 'Capital de La Alianza',
+                id: '000', nombre: 'Ventormenta', descripcion: 'Capital de La Alianza'
+            },
+            {
                 id: '010', nombre: 'Orgrimmar', descripcion: 'Capital de La Horda'
             }
         ]
     })
-})
+}) */
 
 module.exports = router
--- xpress.js
+++ xpress.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+// Conexión a la BBDD
+const mongoose = require('mongoose')
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.fwr7p.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+mongoose.connect(uri,
+    {useNewUrlParser: true, useUnifiedTopology: true}
+    )
+    .then(() => console.log('Base de datos conectada'))
+    .then(e => console.log(e))
+
 // Motor de plantillas
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -20,7 +34,7 @@ app.use((req, res, next) => {
     })
 })
 
-const port = process.env.PORT || 3000
+
 app.listen(port, () => {
     console.log('Servidor en servicio en el puerto: ', port)
 })
